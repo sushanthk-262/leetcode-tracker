916d42f8b13755556902650e10689010e0854975
sushanth <sushanth.kulkarni262@gmail.com>
2025-03-12 11:05:08 +0530

    feat: refactor getUserSubmissions method to use String.format for query construction

--- leetcode-tracker-backend/src/main/java/com/leetcode/tracker/controller/SubmissionController.java
+++ leetcode-tracker-backend/src/main/java/com/leetcode/tracker/controller/SubmissionController.java
@@ -8,28 +8,35 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/submissions")
 public class SubmissionController {
-  // Create an instance of the authentication service.
   private final LeetCodeAuthService authService = new LeetCodeAuthService();
 
-  // Endpoint to log in to LeetCode and store the session cookies.
   @PostMapping("/login")
   public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
     authService.loginToLeetCode(username, password);
     return ResponseEntity.ok("Login successful");
   }
 
-  // Endpoint to fetch submissions for a given LeetCode username.
   @GetMapping("/{username}")
-    public ResponseEntity<String> getUserSubmissions(@PathVariable String username) {
-        String query = """
-        {
-          "query": "query recentSubmissions($username: String!) { recentSubmissionList(username: $username) { title statusDisplay lang timestamp } }",
-          "variables": { "username": \"""" + username + """\" }
-        }""";
+  public ResponseEntity<String> getUserSubmissions(@PathVariable String username) {
+    String query = String.format(
+        """
+            {
+              "query": "query recentSubmissions($username: String!) { recentSubmissionList(username: $username) { title statusDisplay lang timestamp } }",
+              "variables": { "username": "%s" }
+            }
+            """,
+        username);
 
-  HttpHeaders headers = authService.getAuthenticatedHeaders();
-  HttpEntity<String> entity = new HttpEntity<>(query, headers);
-  RestTemplate restTemplate = new RestTemplate();
+    HttpHeaders headers = authService.getAuthenticatedHeaders();
+    HttpEntity<String> entity = new HttpEntity<>(query, headers);
+    RestTemplate restTemplate = new RestTemplate();
 
-  ResponseEntity<String> response = restTemplate.exchange("https://leetcode.com/graphql", HttpMethod.POST, entity, String.class);return ResponseEntity.ok(response.getBody());
-}}
+    ResponseEntity<String> response = restTemplate.exchange(
+        "https://leetcode.com/graphql",
+        HttpMethod.POST,
+        entity,
+        String.class);
+
+    return ResponseEntity.ok(response.getBody());
+  }
+}
