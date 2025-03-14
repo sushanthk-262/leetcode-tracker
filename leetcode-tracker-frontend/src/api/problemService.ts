b62ff9d07c96b3e432690c9ad304b9394d2bcf1b
sushanth <sushanth.kulkarni262@gmail.com>
2025-03-14 20:26:00 +0530

    feat: add functions to fetch problem details by titleSlug and submission details by submissionId

--- leetcode-tracker-frontend/src/api/problemService.ts
+++ leetcode-tracker-frontend/src/api/problemService.ts
@@ -1,5 +1,15 @@
 import axios from 'axios';
+
 export const fetchProblems = async () => {
     const response = await axios.get('http://localhost:8080/problems');
     return response.data;
+};
+
+export const fetchProblemDetails = async (titleSlug: string) => {
+    try {
+        const response = await axios.get(`http://localhost:8080/submissions/question/${titleSlug}`);
+        return response.data;
+    } catch (error) {
+        throw new Error("Failed to fetch problem details");
+    }
 };
--- leetcode-tracker-frontend/src/api/submissionService.ts
+++ leetcode-tracker-frontend/src/api/submissionService.ts
@@ -8,3 +8,12 @@ export const fetchUserSubmissions = async (username: string) => {
         throw new Error("Failed to fetch submissions");
     }
 };
+
+export const fetchSubmissionDetail = async (submissionId: string) => {
+    try {
+        const response = await axios.get(`http://localhost:8080/submissions/submission-detail/${submissionId}`);
+        return response.data;
+    } catch (error) {
+        throw new Error("Failed to fetch submission detail");
+    }
+};
