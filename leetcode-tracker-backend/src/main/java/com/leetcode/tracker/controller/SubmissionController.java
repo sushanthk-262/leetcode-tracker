595f29d4478bc1353cec876c46414d42b43d510a
sushanth <sushanth.kulkarni262@gmail.com>
2025-03-14 20:23:23 +0530

    feat: add endpoint to fetch problem details(difficulty) by titleSlug in SubmissionController

--- leetcode-tracker-backend/src/main/java/com/leetcode/tracker/controller/SubmissionController.java
+++ leetcode-tracker-backend/src/main/java/com/leetcode/tracker/controller/SubmissionController.java
@@ -32,4 +32,22 @@ public class SubmissionController {
 
     return ResponseEntity.ok(response.getBody());
   }
+
+  @GetMapping("/question/{titleSlug}")
+  public ResponseEntity<String> getProblemDetails(@PathVariable String titleSlug) {
+    String query = String.format(
+        "{ \"query\": \"query questionData($titleSlug: String!) { question(titleSlug: $titleSlug) { difficulty } }\", \"variables\": { \"titleSlug\": \"%s\" } }",
+        titleSlug);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<String> entity = new HttpEntity<>(query, headers);
+    RestTemplate restTemplate = new RestTemplate();
+
+    ResponseEntity<String> response = restTemplate.exchange(
+        "https://leetcode.com/graphql", HttpMethod.POST, entity, String.class);
+
+    return ResponseEntity.ok(response.getBody());
+  }
+
 }
